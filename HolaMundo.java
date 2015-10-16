6ce7b04fbbebb7d55baa51857ddcb722a93ebe5d
Mauricio Cano <lcanogiraldo@gmail.com>
2015-10-16 13:22:07 -0500

    Código con mensaje 'Hola Mundo' completado

---
+++ HolaMundo.java
@@ -0,0 +1,8 @@
+//En esta clase vamos a escribir tal cual como nos enseñaron
+public class HolaMundo {
+
+	public static void main(String[] nombre_mi_variable) {
+		System.out.println("Hola Mundo :);");
+	}
+
+}
